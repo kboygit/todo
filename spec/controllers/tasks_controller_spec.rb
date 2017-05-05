0a2bd0adb45ecafa824a4ddc5f639589f4ca4e84
kboygit <heybisayangdako@gmail.com>
2017-05-05 16:59:56 +0000

    loaded the tasks onto the page using dom, installed neatjson for pretty json formatting

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'neatjson'
 
 RSpec.describe TasksController, type: :controller do
   describe 'tasks#index' do
@@ -7,7 +8,8 @@ RSpec.describe TasksController, type: :controller do
       task2 = FactoryGirl.create(:task)
       get :index
       expect(response).to have_http_status :success
-      response_value = ActiveSupport::JSON.decode(@response.body)
+      # response_value = ActiveSupport::JSON.decode(@response.body)
+      response_value = ActiveSupport::JSON.neat_generate(@response.body)
       expect(response_value.count).to eq(2)
     end
   end
